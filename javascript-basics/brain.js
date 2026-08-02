dcb373dd16738af8bbd233cb42e6fe9dcfcd9a83
Ameer Hamza Bugti <223278930+hamzafullstack@users.noreply.github.com>
2026-08-02 09:06:29 +0500

    added javascript basics

---
+++ javascript-basics/brain.js
@@ -0,0 +1,32 @@
+// javascript goes here 
+console.log("hello world");
+console.log('Hello worldddddd!');
+
+// variables
+// These are the building blocks of any program. You can think of variables as “storage containers” for data in your code.
+let userName = "hamza";
+let userAge = 26;
+console.log('sobdar');
+console.log(userName);
+console.log(userAge);
+// You can also re-assign variables:
+userAge = 20;
+console.log(userAge);
+
+// constant : assign once, you cant reassign in const
+const pi = 3.14;
+// pi = 3.14 //error because you cant reassign in const.
+
+// So in summary, there are two ways to declare a variable:
+// let, which we can re-assign.
+// const which we can’t re-assign and will throw an error if we try.
+// there is an old way called var which isnt recommand to use 
+
+// Numbers
+// Numbers are the building blocks of programming logic! In fact, it’s hard to think of any useful programming task that doesn’t involve at least a little basic math,
+// For instance, JavaScript follows the standard mathematical Order of Operations (often remembered by acronyms like PEMDAS or BODMAS).
+// Parentheses (or Brackets) are evaluated first.
+// Exponentiation is done next, from right to left.
+// Multiplication and Division are done next, from left to right.
+// Addition and Subtraction are done last, from left to right.
+console.log((3 + 2) - 76 * (1 + 1)); // perfectly valid syntax
