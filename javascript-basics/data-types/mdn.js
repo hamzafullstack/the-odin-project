9ad61e1d148570a813031f053b8ffdac0857f0d2
Ameer Hamza Bugti <223278930+hamzafullstack@users.noreply.github.com>
2026-08-04 10:44:13 +0500

    Learn: added mdn links and example code

--- javascript-basics/data-types/mdn.js
+++ javascript-basics/data-types/mdn.js
@@ -31,3 +31,9 @@ console.log(joined); // "Hello, how are you?"
 
 
 // note for future hamza: you can find all the resources on mdn-docs..
+
+// more on MDN-docs about the strings.. 
+// source link : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
+const string1 = "A string primitive";
+const string2 = 'Also a string primitive';
+const string3 = `Yet another string primitive`;
