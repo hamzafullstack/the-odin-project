9e2fafe9bc7308f86926c24e228e21f3a851f474
Ameer Hamza Bugti <223278930+hamzafullstack@users.noreply.github.com>
2026-08-10 14:26:41 +0500

    learn: added arrow => function expressions

--- javascript-basics/function-basics/mdn-arrow-functions.js
+++ javascript-basics/function-basics/mdn-arrow-functions.js
@@ -75,3 +75,61 @@ const latina = (a) => a + 9; // latina is variable.
 
 // Function body
 // Arrow functions can have either an expression body or the usual block body.
+
+// Expression body
+const add = (a, b) => a + b; // Implicitly returns a + b
+
+// Block body
+const add2 = (a, b) => {
+  console.log(a, b);
+  return a + b; // Must explicitly return a value
+};
+
+// No return value
+const add3 = (b) => {
+  a += b;
+  // No return statement, so returns undefined
+};
+
+// wrap the object literal in parentheses:
+const func = () => ({ foo: 1 });
+
+// Cannot be used as methods
+// Arrow function expressions should only be used for non-method functions.
+
+// No binding of arguments
+// Arrow functions do not have their own arguments object. Thus, in this example, arguments is a reference to the arguments of the enclosing scope:
+
+function foo(n) {
+  const f = () => arguments[0] + n; // foo is implicit arg binding.
+  return f();
+}
+foo(3);
+
+// In most cases, using rest parameters is a good alternative to using an arguments object.
+
+function foo1(n) {
+  const f1 = (...args) => args[0] + n;
+  return f1(10);
+}
+foo1(1); //11..
+
+// Cannot be used as constructors
+// arrow function can't be used as constructors.
+
+// Cannot be used as generators
+// The yield keyword cannot be used in an arrow function's body
+
+// Line break before arrow
+// An arrow function cannot contain a line break between its parameters and its arrow.
+// you can use ( ) for line break 
+
+// Precedence of arrow
+// Although the arrow in an arrow function is not an operator, arrow functions have special parsing rules that interact differently.
+// => has a lower precedence than most operators.
+
+// where you use arrow function?
+// the greatest benefit of using arrow functions is with methods like setTimeout() and EventTarget.prototype.addEventListener() that usually require some kind of closure, call(), apply(), or bind() to ensure that the function is executed in the proper scope.
+
+
+// for better understanding visit mdn-docs its heaven for web developers.. you will feel like you are at home ❤️❤️❤️
