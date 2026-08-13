20e361334563f95706a65468a1ea82a079a36a1e
Ameer Hamza Bugti <223278930+hamzafullstack@users.noreply.github.com>
2026-08-13 10:26:10 +0500

    learn: add TOP problem solving notes

---
+++ javascript-basics/problem-solving/topProblemSolving.js
@@ -0,0 +1,25 @@
+// this file contains notes and example code of top~(the odin project)'s problem solving lesson.
+// https://www.theodinproject.com/lessons/foundations-problem-solving
+
+// Problem solving is the core thing software developers do. The programming languages and tools they use are secondary to this fundamental skill.
+
+// Problem solving is writing an original program that performs a particular set of tasks and meets all stated constraints. "Think Like A Programmer by V. Anton"
+
+// Q: what is best way to improve your problem solving ability?
+// A: The best way to improve your problem solving ability is by building experience by making lots and lots of programs. The more practice you have the better you’ll be prepared to solve real world problems.
+console.log('The best way to improve your problem solving ability is by building experience by making lots and lots of programs. The more practice you have the better you’ll be prepared to solve real world problems.'); // keep in mind.
+
+// Understand the problem
+// The first step to solving a problem is understanding exactly what the problem is.
+
+// To gain clarity and understanding of the problem, write it down on paper, reword it in plain English until it makes sense to you, and draw diagrams if that helps. When you can explain the problem to someone else in plain English, you understand it.
+
+// Plan
+// Some of the questions you should answer at this stage of the process:
+
+// Does your program have a user interface? What will it look like? What functionality will the interface have? Sketch this out on paper.
+// What inputs will your program have? Will the user enter data or will you get input from somewhere else?
+// What’s the desired output?
+// Given your inputs, what are the steps necessary to return the desired output?
+
+// The last question is where you will write out an algorithm to solve the problem. You can think of an algorithm as a recipe for solving a particular problem. It defines the steps that need to be taken by the computer to solve a problem in pseudocode
