de3f6984a00ab6d97a1ee05c0b5b997d726fae0a
Ameer Hamza Bugti <223278930+hamzafullstack@users.noreply.github.com>
2026-08-14 11:01:02 +0500

    learn: add the odin project understanding errors notes

---
+++ javascript-basics/understanding-errors/topErrorHandling.js
@@ -0,0 +1,43 @@
+// Understanding Errors
+// https://www.theodinproject.com/lessons/foundations-understanding-errors
+
+// Reading and understanding error messages is a requirement as a developer.
+
+// However, error messages provide developers with a treasure trove of knowledge, and tell you everything you need to know about how to resolve them! Being able to parse error messages and warnings without fear will enable you to effectively debug your applications, receive meaningful help from others, and empower yourself to push forward when faced with an error.
+
+// The anatomy of an error
+// An error is a type of object built into the JS language, consisting of a name/type and a message. 
+
+const a = "Hello";
+const b = "World";
+
+console.log(c);
+// Reference Error.
+// A ReferenceError is thrown when one refers to a variable that is not declared and/or initialized within the current scope. In our case, the error message explains that the error has occurred because c is not defined.
+
+// Common types of errors
+// These are some of the most common errors you will encounter, so it’s important to understand them.
+
+// syntax error.
+// A syntax error occurs when the code you are trying to run is not written correctly, i.e., in accordance with the grammatical rules of JavaScript.
+
+// Reference error
+// We covered reference errors in the first example in this lesson, but it’s important to remember that these arise because whatever variable you are trying to reference does not exist (within the current scope) - or it has been spelled incorrectly!
+
+// Type error
+// Per MDN, a TypeError may be thrown when:
+// an operand or argument passed to a function is incompatible with the type expected by that operator or function;
+// or when attempting to modify a value that cannot be changed;
+// or when attempting to use a value in an inappropriate way.
+
+// You can learn more about the errors on google or on the following article with its refered links 
+
+// Assignment.
+// MDN-docs : documentation, where you can read more about errors..
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/ReferenceError
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/SyntaxError
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/TypeError
+// https://developer.mozilla.org/en-US/docs/Learn_web_development/Core/Scripting/What_went_wrong
+
+
+// ###### HAPPY INDEPENDECE DAY ---- PAKISTAN ####### PAKISTAN ZINDABAD ❤️
