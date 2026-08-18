d6030604d3ba2b31ee25dbf3530b4ad94613993b
Ameer Hamza Bugti <223278930+hamzafullstack@users.noreply.github.com>
2026-08-18 09:45:24 +0500

    learn: add javascript loops short notes from T.O.P

---
+++ javascript-basics/loops-and-arrays/topLoops.js
@@ -0,0 +1,17 @@
+// https://www.theodinproject.com/lessons/foundations-loops-and-arrays
+// Loops and arrays on The odin project..
+
+// intro 
+// One of the challenges of programming is repetition.
+// and we  often want that repeated tasks should be readabe and shorts thats where loops comes in use..
+// in this lesson  i am about to learn loops. loops are control structure that allow you to execute a block of code repeatedly..
+
+// loops: 
+// computers  dont get tired and they are really fast, and can perfom tasks thousand and even millions of times in just few short seconds.
+// To repeat a set of instructions multiple times we can use a loop.
+
+// go through to following resources to learn about loops.
+
+// https://developer.mozilla.org/en-US/docs/Learn_web_development/Core/Scripting/Loops
+
+// https://javascript.info/while-for
