f6ac8558f3fe58ec498f6d1adbce14472eeb486b
Ameer Hamza Bugti <223278930+hamzafullstack@users.noreply.github.com>
2026-08-21 12:27:18 +0500

    learn: add TOP introduction to Arrays notes and resources

---
+++ javascript-basics/loops-and-arrays/top-intro-to-arrays.js
@@ -0,0 +1,13 @@
+// https://www.theodinproject.com/lessons/foundations-loops-and-arrays#arrays
+
+// The odin project Intro to Arrays
+// arrays are data structures in javascript.
+// Strings and numbers may be our building blocks, but as your scripts get more complex, you’re going to need a way to deal with large quantities of them. this is where other data types comes in use, Arrays are very useful and being used widely in any kind of development like Game, web, software, etc.
+
+
+// learn more on following links and then come back here again. 
+// https://www.youtube.com/watch?v=7W4pQQ20nJg
+// https://javascript.info/array
+// https://javascript.info/array-methods
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+
