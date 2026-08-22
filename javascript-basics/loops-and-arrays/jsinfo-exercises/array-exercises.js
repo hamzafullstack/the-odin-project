b7ee85aa2534f2c136698fd3b414603f440e7d34
Ameer Hamza Bugti <223278930+hamzafullstack@users.noreply.github.com>
2026-08-22 13:11:23 +0500

    learn: add javascript.info array exercises solutions example code

---
+++ javascript-basics/loops-and-arrays/jsinfo-exercises/array-exercises.js
@@ -0,0 +1,24 @@
+// https://javascript.info/array#tasks
+
+// javascript.info array exercise solutions. 
+
+// Task~~1 Is array copied?
+// result will be 4.
+
+// Task~~2 Array operations.
+let styles = ['Jazz', 'Blue']; // created an array
+console.log(styles); // printing the base array.
+styles.push('Rock-N-Roll'); // appended at the end.
+console.log(styles); // printing to check results
+styles.splice(1, 1, 'Classics'); // replaces the items according to W3schools.com.
+console.log(styles);
+styles.shift(); // removes first item
+console.log(styles);
+styles.unshift('Rap', 'Reggae'); // append at starting
+console.log(styles);
+
+// Task~~3 Calling in an array context
+// result will Array items + anonymous function, 
+
+// Task~~4 Sum input numbers
+// this solution requires an html page so i will solve in the html file.
