dd6661e3cef3e6b3310851f7081181812433e97a
Ameer Hamza Bugti <223278930+hamzafullstack@users.noreply.github.com>
2026-08-25 13:23:51 +0500

    feat: add camelize function using split, map, and join

--- javascript-basics/loops-and-arrays/jsinfo-exercises/top-assignments/task-one.js
+++ javascript-basics/loops-and-arrays/jsinfo-exercises/top-assignments/task-one.js
@@ -1,5 +1,6 @@
 // Translate border-left-width to borderLeftWidth
 
+// solution 1 ~ i solved then i realized exercise says use the array methods like split, transform and join..
 function camelize(str) {
     return str.toLowerCase().replace(/[^a-zA-Z0-9]+(.)/g, (match, char) => char.toUpperCase());
 }
@@ -8,3 +9,14 @@ console.log(camelize('background-color-white'));
 console.log(camelize('my-short-string'));
 console.log(camelize('my short string'));
 
+// Solution 2 ~ where i used split, map and join methods.
+function camelCase(string) {
+    return string.split(/[-]/).map((word, index) => {
+        if(index === 0) {
+            return word;
+        }
+        return (word.charAt(0).toUpperCase() + word.slice(1));
+    }).join("");
+}
+console.log(camelCase('my-short-string'));
+console.log(camelCase('hello-my-world'));
