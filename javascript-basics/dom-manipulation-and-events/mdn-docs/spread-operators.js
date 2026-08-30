72d4089b8e312953223376cfa18c29a5c1fe4636
Ameer Hamza Bugti <223278930+hamzafullstack@users.noreply.github.com>
2026-08-30 10:09:39 +0500

    learn: add mdn-docs spread operator notes

---
+++ javascript-basics/dom-manipulation-and-events/mdn-docs/spread-operators.js
@@ -0,0 +1,9 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax
+
+// Spread syntax (...)
+// the spread syntax allows an iterable, such as an array or string to be expanded in places where zero or more arguments for (function calls) or elements(for array literals) are excepted. in an object literal, the spread enumerates the properties of an object and adds the key value pairs to the object being created.
+
+// spread syntax looks exactly like rest syntax.
+// spread syntax is the opposite of rest syntax.
+// spread syntax === expands (unpack).
+// rest syntax === collects (collection)
