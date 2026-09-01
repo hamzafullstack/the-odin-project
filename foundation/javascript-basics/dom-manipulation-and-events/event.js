6ca5b69bf22cc81dc034713dfac827a4a76c3997
Ameer Hamza Bugti <223278930+hamzafullstack@users.noreply.github.com>
2026-09-01 15:05:58 +0500

    add javascript events example code and notes

--- foundation/javascript-basics/dom-manipulation-and-events/event.js
+++ foundation/javascript-basics/dom-manipulation-and-events/event.js
@@ -10,3 +10,37 @@ btn.onclick = () => alert('Hello World!');
 // https://javascript.info/arrow-functions-basics
 
 let sum = (a, b) => a + b; // arrow function.
+
+// its better  use addEventListener() because its modern.
+const btn2 = document.querySelector('#btn2');
+btn2.addEventListener("click", () => { console.log('Hello Dickhead'); });
+
+// regular function 
+function command() {
+    alert("Pant's down, spread your legs bitch.");
+}
+// selecting third button using his id in q/s().
+const btn3 = document.querySelector('#btn3');
+
+// old method onclick..
+btn3.onclcik = command;
+
+// new method addEventListener()
+btn3.addEventListener('click', command);
+
+// using named functions can clean up your code considerably, and is a really good idea if the function is something that you are going to want to do in multiple places..
+
+const btn4 = addEventListener('#btn4');
+// passing param to the function that we are calling
+btn4.addEventListener('click', function (smile) {
+    console.log(smile);
+});
+
+// understanding callbacks 
+// when we pass in command or function (smile){...} as an argument to addEventListener, we call this a callback.A callback is simply a function that is passed into another function as an argument.
+
+// the  smile parameter in that callback function contains an object that references the event itself. 
+// javascript knows the parameter is an event because an eventlistener callback takes an event object by definition.
+
+// learn more about event objects on MDN-docs 
+// https://developer.mozilla.org/en-US/docs/Learn_web_development/Core/Scripting/Events#event_objects
