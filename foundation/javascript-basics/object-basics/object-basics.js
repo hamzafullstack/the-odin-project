205f5e8e32e18bd24d0bd0b7715b50005f4e5f1f
Ameer Hamza Bugti <223278930+hamzafullstack@users.noreply.github.com>
2026-09-14 14:31:34 +0500

    introduction to javascript objects

---
+++ foundation/javascript-basics/object-basics/object-basics.js
@@ -0,0 +1,11 @@
+// https://www.theodinproject.com/lessons/foundations-object-basics
+
+// The Odin Project - Object basics
+
+// this is the last lesson of foundation course, 
+// in this lesson i'll learn objects - a collection of key-value pairs - as well as some more powerful and commonly used array methods.
+
+// Object and arrays are really powerful and widely used in javascript.
+
+// Objects
+// objects are a very important part of the javascript language, and while for the most of part you can accomplish simple and even intermediate tasks without worrying about them. almost every real project is going to feature objects.
